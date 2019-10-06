838503ace279be8f7ff403831df89f4362f21236
Gusti Sadhu J.W <32070030+Uthea@users.noreply.github.com>
2019-10-06 23:44:05 +0700

    Initial Commit

---
+++ test_lemma.py
@@ -0,0 +1,37 @@
+from lemmatizer import Lemmatizer
+
+lemma = Lemmatizer()
+
+
+print(
+lemma.lemmatize('bersetubuh'),
+lemma.lemmatize('berdansa'),
+lemma.lemmatize('penamaan'),
+lemma.lemmatize('berusaha'),
+lemma.lemmatize('berdansa'),
+lemma.lemmatize('bolak-balik'),
+lemma.lemmatize('gemetar'),
+lemma.lemmatize('petanggungjawaban'),
+lemma.lemmatize('kepastian'),
+lemma.lemmatize('berpendidikan'),
+lemma.lemmatize('berhubungan'),
+lemma.lemmatize('berwawasan'),
+lemma.lemmatize('pengetahuan'),
+lemma.lemmatize('pengembala'), 
+lemma.lemmatize('penarikan'),
+lemma.lemmatize('terbengkalai'),
+lemma.lemmatize('rumahku'),
+lemma.lemmatize('penanggulangan'),
+lemma.lemmatize('perpecahan'),
+lemma.lemmatize('pemalas'),
+lemma.lemmatize('tertikunganlah'),
+lemma.lemmatize('perdamaian'),
+lemma.lemmatize('terbirit-birit'), 
+lemma.lemmatize('cebokan'),
+lemma.lemmatize('mengotomatisasikan'), 
+lemma.lemmatize('menyelesaikan'), 
+lemma.lemmatize('sekawasan'), 
+lemma.lemmatize('pengertian'),
+lemma.lemmatize('ketidakpastian')  
+)
+
